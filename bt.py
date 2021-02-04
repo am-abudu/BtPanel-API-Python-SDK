4b362f7a09d704fb5bc0f9c33ab50abc654965ed
am-abudu <51912589+am-abudu@users.noreply.github.com>
2021-02-04 21:24:17 +0800

    Update bt.py

--- bt.py
+++ bt.py
@@ -13,6 +13,7 @@
  * @ 参考 阿良 or Youngxj(二次开发)
  * @ 官方API文档 https://www.bt.cn/api-doc.pdf
  * @ 博客 https://www.abudu.top
+ * @ Github https://github.com/am-abudu/BtPanel-API-Python-SDK
  * @ 版本 0.1
 """
 
